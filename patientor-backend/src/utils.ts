fdcdd9e49465cfa64aae3ea02419e33c678b1d9f
RotcivL <victor.lee.98.vic@gmail.com>
2023-02-21 15:56:19 +1100

    Ex.9.13

---
+++ patientor-backend/src/utils.ts
@@ -0,0 +1,62 @@
+import { NewPaitent, Gender } from './types';
+
+const isString = (text: unknown): text is string => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+const parseString = (param: unknown, paramName: string): string => {
+  if (!param || !isString(param)) {
+    throw new Error(`Incorrect or missing ${paramName}`);
+  }
+
+  return param;
+};
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+const parseDate = (date: unknown): string => {
+  if (!date || !isString(date) || !isDate(date)) {
+    throw new Error('Incorrect or missing date: ' + date);
+  }
+  return date;
+};
+
+const isGender = (param: string): param is Gender => {
+  return Object.values(Gender)
+    .map(v => v.toString())
+    .includes(param);
+};
+
+const parseGender = (gender: unknown): Gender => {
+  if (!gender || !isString(gender) || !isGender(gender)) {
+    throw new Error('Incorrect or missing weather: ' + gender);
+  }
+  return gender;
+};
+
+const toNewPatient = (object: unknown): NewPaitent => {
+  if (!object || typeof object !== 'object') {
+    throw new Error('Incorrect or missing data');
+  }
+  if (
+    'name' in object &&
+    'dateOfBirth' in object &&
+    'ssn' in object &&
+    'gender' in object &&
+    'occupation' in object
+  ) {
+    const newEntry: NewPaitent = {
+      name: parseString(object.name, 'name'),
+      dateOfBirth: parseDate(object.dateOfBirth),
+      ssn: parseString(object.ssn, 'ssn'),
+      gender: parseGender(object.gender),
+      occupation: parseString(object.occupation, 'occupation'),
+    };
+    return newEntry;
+  }
+  throw new Error('Incorrect data: some fields are missing');
+};
+
+export default toNewPatient;
